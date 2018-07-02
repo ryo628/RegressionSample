d40bad107939ffd081bd5ba994a6c64d8dbc6afb
ryo628 <ryokirito628@gmail.com>
2018-07-02 14:23:04 +0900

    all params allowd

--- include/Food.hpp
+++ include/Food.hpp
@@ -10,17 +10,22 @@ private:
 	double carbon;
 	double fat;
 	double calorie;
+	double gi;
+	double protain;
 
 public:
 	/* constructor */
 	Food();
 	Food( std::string s, double d1, double d2, double d3 );
+	Food( std::string s, double d1, double d2, double d3, double d4, double d5 );
 
 	/* accessor */
 	std::string getName();
 	double getCarbon();
 	double getFat();
 	double getCalorie();
+	double getGI();
+	double getProtain();
 };
 
 #endif
--- source/Food.cpp
+++ source/Food.cpp
@@ -13,6 +13,17 @@ Food::Food( std::string s, double d1, double d2, double d3 )
 	this->calorie	= d3;
 }
 
+Food::Food( std::string s, double d1, double d2, double d3, double d4, double d5 )
+{
+	this->name		= s;
+	this->carbon	= d1;
+	this->fat		= d2;
+	this->gi		= d3;
+	this->protain	= d4;
+	this->calorie	= d5;
+}
+
+
 std::string Food::getName()
 {
 	return this->name;
@@ -31,4 +42,14 @@ double Food::getFat()
 double Food::getCalorie()
 {
 	return this->calorie;
+}
+
+double Food::getGI()
+{
+	return this->gi;
+}
+
+double Food::getProtain()
+{
+	return this->protain;
 }
